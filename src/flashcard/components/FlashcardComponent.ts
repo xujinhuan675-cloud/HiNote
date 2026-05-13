78971afcafb40c914a9b0157ab4e65b83a760f27
CatMuse <kairos.jiang@gmail.com>
2026-05-13 14:29:02 +0800

    Extract flashcard UI state helpers

--- src/flashcard/components/FlashcardComponent.ts
+++ src/flashcard/components/FlashcardComponent.ts
@@ -5,8 +5,6 @@ import {
     FlashcardState, 
     FSRS_RATING, 
     FSRSRating, 
-    CardGroup,
-    FlashcardProgress,
     GroupProgressState
 } from "../types/FSRSTypes";
 import { t } from "../../i18n";
@@ -20,6 +18,14 @@ import {
 } from "./controllers";
 import type CommentPlugin from "../../../main";
 import type { FlashcardRatingButton } from "./FlashcardComponentContext";
+import {
+    findGroupIdByName,
+    findGroupNameById,
+    getGroupCompletionMessage,
+    loadFlashcardUIState,
+    saveFlashcardUIState,
+    setGroupCompletionMessage
+} from "./FlashcardUIState";
 
 /**
  * 闪卡组件，整合所有闪卡相关功能
@@ -34,7 +40,6 @@ export class FlashcardComponent extends Component {
     private isActive: boolean = false;
     private licenseManager: LicenseManager;
     private fsrsManager: FSRSManager;
-    private currentCard: FlashcardState | null = null;
     private currentGroupName: string = '';
     private currentGroupId: string = '';
     private app: App;
@@ -71,56 +76,13 @@ export class FlashcardComponent extends Component {
         this.progressManager = new FlashcardProgressManager(this);
         this.utils = new FlashcardUtils(this);
         
-        // 初始化属性
-        this.groupProgress = {};
-        
-        // 加载 UI 状态
-        const uiState = this.fsrsManager.getUIState() || {};
-        
-        // 设置当前分组名称
-        this.currentGroupName = uiState.currentGroupName || '';
-        
-        // 设置当前分组 ID
-        if (this.currentGroupName) {
-            const group = this.fsrsManager.getCardGroups().find((g: CardGroup) => g.name === this.currentGroupName);
-            if (group) {
-                this.currentGroupId = group.id;
-            } else {
-                this.currentGroupId = '';
-            }
-        } else {
-            this.currentGroupId = '';
-        }
-        
-        // 设置其他状态
-        this.completionMessage = uiState.completionMessage || null;
-        
-        // 初始化分组进度
-        if (uiState.groupProgress) {
-            this.groupProgress = { ...uiState.groupProgress };
-        }
-        
-        // 不再需要将完成消息存储到单独的对象中，直接从 groupProgress 读取
-        
-        // 从分组进度中获取当前索引和翻转状态
-        if (this.currentGroupName && this.groupProgress[this.currentGroupName]) {
-            this.currentIndex = this.groupProgress[this.currentGroupName].currentIndex || 0;
-            this.isFlipped = this.groupProgress[this.currentGroupName].isFlipped || false;
-        } else {
-            this.currentIndex = 0;
-            this.isFlipped = false;
-        }
-        
-        // 确保 groupProgress 是对象
-        if (!uiState || !uiState.groupProgress || typeof uiState.groupProgress !== 'object') {
-            this.groupProgress = {};
-        }
-        
-        if (uiState && 'groupProgress' in uiState && uiState.groupProgress && typeof uiState.groupProgress === 'object') {
-            this.groupProgress = { ...uiState.groupProgress };
-        } else {
-            this.groupProgress = {};
-        }
+        const uiState = loadFlashcardUIState(this.fsrsManager);
+        this.currentGroupName = uiState.currentGroupName;
+        this.currentGroupId = uiState.currentGroupId;
+        this.currentIndex = uiState.currentIndex;
+        this.isFlipped = uiState.isFlipped;
+        this.completionMessage = uiState.completionMessage;
+        this.groupProgress = uiState.groupProgress;
     }
     
     /**
@@ -246,17 +208,7 @@ export class FlashcardComponent extends Component {
     
     public setCurrentGroupName(groupName: string) {
         this.currentGroupName = groupName;
-        // 更新分组 ID
-        if (groupName) {
-            const group = this.fsrsManager.getCardGroups().find((g: CardGroup) => g.name === groupName);
-            if (group) {
-                this.currentGroupId = group.id;
-            } else {
-                this.currentGroupId = '';
-            }
-        } else {
-            this.currentGroupId = '';
-        }
+        this.currentGroupId = findGroupIdByName(this.fsrsManager, groupName);
     }
     
     public getCurrentGroupId(): string {
@@ -265,13 +217,7 @@ export class FlashcardComponent extends Component {
     
     public setCurrentGroupId(groupId: string) {
         this.currentGroupId = groupId;
-        // 更新分组名称
-        if (groupId) {
-            const group = this.fsrsManager.getCardGroups().find((g: CardGroup) => g.id === groupId);
-            this.currentGroupName = group ? group.name : '';
-        } else {
-            this.currentGroupName = '';
-        }
+        this.currentGroupName = findGroupNameById(this.fsrsManager, groupId);
     }
     
     /**
@@ -296,10 +242,7 @@ export class FlashcardComponent extends Component {
      * @returns 完成消息或 null
      */
     public getGroupCompletionMessage(groupName: string): string | null {
-        if (this.groupProgress[groupName]) {
-            return this.groupProgress[groupName].completionMessage || null;
-        }
-        return null;
+        return getGroupCompletionMessage(this.groupProgress, groupName);
     }
     
     /**
@@ -308,47 +251,7 @@ export class FlashcardComponent extends Component {
      * @param message 完成消息或 null
      */
     public setGroupCompletionMessage(groupName: string, message: string | null) {
-        // 确保分组进度对象存在
-        if (!this.groupProgress[groupName]) {
-            this.groupProgress[groupName] = {
-                currentIndex: 0,
-                isFlipped: false,
-                completionMessage: null
-            };
-        }
-        
-        // 设置完成消息
-        this.groupProgress[groupName].completionMessage = message;
-        
-        // 同步更新到 FSRSManager 的存储中
-        if (this.fsrsManager) {
-            const uiState = this.fsrsManager.getUIState();
-            if (!uiState.groupProgress) {
-                uiState.groupProgress = {};
-            }
-            
-            // 确保分组进度对象存在
-            if (!uiState.groupProgress[groupName]) {
-                uiState.groupProgress[groupName] = {
-                    currentIndex: 0,
-                    isFlipped: false,
-                    currentCardId: undefined,
-                    completionMessage: undefined
-                };
-            }
-            
-            // 更新完成消息
-            uiState.groupProgress[groupName].completionMessage = message;
-            
-            // 如果设置了完成消息，同时重置卡片状态
-            if (message) {
-                uiState.groupProgress[groupName].isFlipped = false;
-                uiState.groupProgress[groupName].currentCardId = undefined;
-            }
-            
-            // 更新 UI 状态
-            this.fsrsManager.updateUIState(uiState);
-        }
+        setGroupCompletionMessage(this.fsrsManager, this.groupProgress, groupName, message);
     }
     
     public getGroupProgress(groupName?: string): GroupProgressState | null {
@@ -399,49 +302,14 @@ export class FlashcardComponent extends Component {
      * 优化版本：直接在组件中处理状态保存，确保所有状态都被正确保存
      */
     public saveState() {
-        // 获取当前分组
-        const groupName = this.getCurrentGroupName();
-        
-        // 获取 UI 状态
-        const uiState = this.fsrsManager.getUIState();
-        
-        // 更新基本 UI 状态
-        uiState.currentGroupName = groupName;
-        uiState.completionMessage = this.completionMessage;
-        
-        // 确保分组进度存在
-        if (!uiState.groupProgress) {
-            uiState.groupProgress = {};
-        }
-        
-        // 只有在有分组名称时才保存分组进度
-        if (groupName) {
-            // 保存当前分组的进度
-            const currentCardId = this.cards.length > 0 && this.currentIndex < this.cards.length ? 
-                this.cards[this.currentIndex].id : undefined;
-            
-            // 获取当前分组的完成消息
-            const completionMessage = this.getGroupCompletionMessage(groupName);
-                
-            // 创建或更新分组进度
-            if (!uiState.groupProgress[groupName]) {
-                uiState.groupProgress[groupName] = {
-                    currentIndex: this.currentIndex,
-                    isFlipped: this.isFlipped,
-                    currentCardId: currentCardId,
-                    completionMessage: completionMessage
-                };
-            } else {
-                // 更新现有分组进度
-                uiState.groupProgress[groupName].currentIndex = this.currentIndex;
-                uiState.groupProgress[groupName].isFlipped = this.isFlipped;
-                uiState.groupProgress[groupName].currentCardId = currentCardId;
-                uiState.groupProgress[groupName].completionMessage = completionMessage;
-            }
-        }
-        
-        // 保存 UI 状态
-        this.fsrsManager.updateUIState(uiState);
+        saveFlashcardUIState(this.fsrsManager, {
+            currentGroupName: this.currentGroupName,
+            completionMessage: this.completionMessage,
+            cards: this.cards,
+            currentIndex: this.currentIndex,
+            isFlipped: this.isFlipped,
+            groupProgress: this.groupProgress
+        });
     }
     
     public updateProgress() {
